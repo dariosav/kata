50714993daca6d193249578610f3bcf1494f55c7
Dario <dario.savarese@agilitaseurope.com>
2019-01-05 02:21:08 +0100

    introduced helper class

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,4 +1,5 @@
 package com.gildedrose;
+import static com.gildedrose.helpers.ItemHelper.*;
 
 class GildedRose {
 	public static final String AGED_BRIE = "Aged Brie";
@@ -15,81 +16,42 @@ class GildedRose {
 
 		for (int i = 0; i < items.length; i++) {
 			Item currentItem = items[i];
-
-			switch (currentItem.name) {
-			case SULFURAS_HAND_OF_RAGNAROS:
-				break;
-			case AGED_BRIE:
-			case BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT:
-			case CONJURED_MANA_CAKE:
-			default:
-				decreaseSellIn(currentItem);
-			}
-
-			switch (currentItem.name) {
-			case SULFURAS_HAND_OF_RAGNAROS:
-				break;
-			case AGED_BRIE:
-				updateQualityBy(currentItem, 1);
-				break;
-			case BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT:
-				if (currentItem.sellIn >= 10) {
-					updateQualityBy(currentItem, +1);
-				}
-				if (currentItem.sellIn >= 5 && currentItem.sellIn < 10) {
-					updateQualityBy(currentItem, +2);
-				}
-				if (currentItem.sellIn >= 0 && currentItem.sellIn < 5) {
-					updateQualityBy(currentItem, +3);
-				}
-				if (currentItem.sellIn < 0) {
-					updateQualityBy(currentItem, -currentItem.quality);
-				}
-				break;
-			case CONJURED_MANA_CAKE:
-				updateQualityBy(currentItem, -2);
-				break;
-			default:
-				updateQualityBy(currentItem, -1);
-			}
-
+			decreaseSellInDay(currentItem);
+			updateQuality(currentItem);
 		}
 	}
 
-	private void updateQualityBy(Item currentItem, int updateValue) {
-		updateValue = adjustQualityValueIfSellInIsPassed(currentItem, updateValue);
-		setNewQualityValue(currentItem, updateValue);
-		checkMinQaulityValue(currentItem);
-		checkMaxQualityValue(currentItem);
-	}
-
-	private void setNewQualityValue(Item currentItem, int updateValue) {
-		currentItem.quality = currentItem.quality + updateValue;
-	}
-
-	/*  The Quality of an item is never more than 50 */
-	private void checkMaxQualityValue(Item currentItem) {
-		if (currentItem.quality > 50) {
-			currentItem.quality = 50;
+	private void updateQuality(Item currentItem) {
+		switch (currentItem.name) {
+		case SULFURAS_HAND_OF_RAGNAROS:
+			break;
+		case AGED_BRIE:
+			updateAgedItemQuality(currentItem);
+			break;
+		case BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT:
+			updateBackStagePassItemQuality(currentItem);
+			break;
+		case CONJURED_MANA_CAKE:
+			updateConjuredItemQauality(currentItem);
+			break;
+		default:
+			updateGenericItem(currentItem);
 		}
 	}
 
-	/* The Quality of an item is never negative */
-	private void checkMinQaulityValue(Item currentItem) {
-		if (currentItem.quality < 0) {
-			currentItem.quality = 0;
+	private void decreaseSellInDay(Item currentItem) {
+		switch (currentItem.name) {
+		case SULFURAS_HAND_OF_RAGNAROS:
+			break;
+		case AGED_BRIE:
+		case BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT:
+		case CONJURED_MANA_CAKE:
+		default:
+			decreaseSellIn(currentItem);
 		}
 	}
 
-    /* Once the sell by date has passed, Quality degrades twice as fast */
-	private int adjustQualityValueIfSellInIsPassed(Item currentItem, int updateValue) {
-		if (currentItem.sellIn < 0 && updateValue < 0) {
-			updateValue = updateValue * 2;
-		}
-		return updateValue;
-	}
 
-	private void decreaseSellIn(Item currentItem) {
-		currentItem.sellIn = currentItem.sellIn - 1;
-	}
+
+
 }
---
+++ src/main/java/com/gildedrose/helpers/ItemHelper.java
@@ -0,0 +1,69 @@
+package com.gildedrose.helpers;
+
+import com.gildedrose.Item;
+
+public class ItemHelper {
+	public static void updateGenericItem(Item currentItem) {
+		updateQualityBy(currentItem, -1);
+	}
+
+	public static void updateConjuredItemQauality(Item currentItem) {
+		updateQualityBy(currentItem, -2);
+	}
+
+	public static void updateBackStagePassItemQuality(Item currentItem) {
+		if (currentItem.sellIn >= 10) {
+			updateQualityBy(currentItem, +1);
+		}
+		if (currentItem.sellIn >= 5 && currentItem.sellIn < 10) {
+			updateQualityBy(currentItem, +2);
+		}
+		if (currentItem.sellIn >= 0 && currentItem.sellIn < 5) {
+			updateQualityBy(currentItem, +3);
+		}
+		if (currentItem.sellIn < 0) {
+			updateQualityBy(currentItem, -currentItem.quality);
+		}
+	}
+	
+	public static void updateAgedItemQuality(Item currentItem) {
+		updateQualityBy(currentItem, 1);
+	}
+
+	private static void updateQualityBy(Item currentItem, int updateValue) {
+		updateValue = adjustQualityValueIfSellInIsPassed(currentItem, updateValue);
+		setNewQualityValue(currentItem, updateValue);
+		checkMinQaulityValue(currentItem);
+		checkMaxQualityValue(currentItem);
+	}
+
+	private static void setNewQualityValue(Item currentItem, int updateValue) {
+		currentItem.quality = currentItem.quality + updateValue;
+	}
+
+	/*  The Quality of an item is never more than 50 */
+	private static void checkMaxQualityValue(Item currentItem) {
+		if (currentItem.quality > 50) {
+			currentItem.quality = 50;
+		}
+	}
+
+	/* The Quality of an item is never negative */
+	private static void checkMinQaulityValue(Item currentItem) {
+		if (currentItem.quality < 0) {
+			currentItem.quality = 0;
+		}
+	}
+
+    /* Once the sell by date has passed, Quality degrades twice as fast */
+	public static int adjustQualityValueIfSellInIsPassed(Item currentItem, int updateValue) {
+		if (currentItem.sellIn < 0 && updateValue < 0) {
+			updateValue = updateValue * 2;
+		}
+		return updateValue;
+	}
+
+	public static void decreaseSellIn(Item currentItem) {
+		currentItem.sellIn = currentItem.sellIn - 1;
+	}
+}
