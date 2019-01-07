91c1b12b3128b9207642808927e13ea5aec4be49
dariosav <46372727+dariosav@users.noreply.github.com>
2019-01-07 10:45:59 +0100

    Update GildedRose.java
    Removed unused constants

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -4,10 +4,7 @@ import com.gildedrose.items.GildedRoseItem;
 import com.gildedrose.items.GildedRoseItemFactory;
 
 class GildedRose {
-	public static final String AGED_BRIE = "Aged Brie";
-	public static final String BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
-	public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
-	public static final String CONJURED_MANA_CAKE = "Conjured Mana Cake";
+
 	Item[] items;
 
 	public GildedRose(Item[] items) {
